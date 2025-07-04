083bceb28f558f2d6fc3fe97a6fdd8841a0fcd82
Tyler Haas <haasdevelopment@gmail.com>
2025-07-03 20:52:02 -0700

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,12 @@
 /// This is the best implementation of this program that ever existed.
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
+// a function that prints a message
+fn print(m: &str) {
+    println!("{m}");
 }
 
-fn print_goodbye() {
-    println!("Goodbye, world!");
-}
