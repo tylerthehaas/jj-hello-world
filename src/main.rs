8df8c8ebbcbba65ec8cdb306365831061c308d79
Tyler Haas <haasdevelopment@gmail.com>
2025-07-03 20:19:38 -0700

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// This is the best implementation of this program that ever existed.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
