8a081682307374b90309dbc511d5afd094a5bde1
Tyler Haas <haasdevelopment@gmail.com>
2025-07-03 22:03:56 -0700

    add a comment to main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-/// A "hello world!" program.
+/// A "Hello, world!" program.
 ///
-/// This is the best implementation of this program that ever existed.
+/// This is the best implementation of this program to ever exist.
 
+/// The main function runs when our program starts
 fn main() {
     print("Hello, world!");
+    print("Goodbye, world!");
 }
 
 // a function that prints a message
 fn print(m: &str) {
-    println!("{m}");
+    println!("{m}")
 }
-
